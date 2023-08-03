6b5cc9461b4caa2f03493fb4b61183807ee8ba2b
riedlermat <riedlerma@gmx.at>
2023-08-03 15:52:40 +0200

    Delete runtimeAppProject/qasmDraw directory

--- runtimeAppProject/qasmDraw/bin/qasmDraw/run_script.bat
+++
@@ -1,2 +0,0 @@
-javac C:\Users\Riedler\Documents\Uni\SS2023\bachelor\runtime-EclipseApplication\qasmDraw\src\qasmDraw\PythonScriptRunner.java
-java -cp C:\Users\Riedler\Documents\Uni\SS2023\bachelor\runtime-EclipseApplication\qasmDraw\src qasmDraw.PythonScriptRunner
--- runtimeAppProject/qasmDraw/bin/test.qasm
+++
@@ -1,8 +0,0 @@
-OPENQASM 2.0;
-include "qelib1.inc";
-qreg q[6];
-cry(pi/4) q[1],q[0];
-cry(3*pi/2) q[0],q[1];
-crx(3*pi/4) q[1],q[0];
-ry(pi/2) q[0];
-cry(pi/2) q[0],q[2];
